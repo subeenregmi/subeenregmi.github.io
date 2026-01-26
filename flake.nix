adde448139df8b6dfaf4bf679b9fc1393f2212b6
Subeen Regmi <subeenregmi123@gmail.com>
2026-01-26 20:26:32 +0000

    direnv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "subeenregmi";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { nixpkgs, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell{
+        name = "sregmi";
+
+        packages = with pkgs; [
+          ruby
+        ];
+
+      };
+    };
+}
